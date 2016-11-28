c14be367e5fdceb1d6ce91aa0e7b69cdbed2e6e3
Maurizio Casimirri <maurizio.casimirri@auctionata.com>
2016-11-28 17:27:18 +0100

    support post as attachments

--- README.md
+++ README.md
@@ -10,3 +10,7 @@ docker run -d -p 4567:4567 mcasimir/htmldiff
 $ curl -w '\n' -d 'html1=<h1>foo</h1>&html2=<h1>bar</h1>' http://localhost:4567/diff
 <h1><del class="diffmod">foo</del><ins class="diffmod">bar</ins></h1>
 ```
+
+```
+$ curl -X POST -F 'html1=@html1.html' -F 'html2=@html2.html' http://localhost:4567/diff
+```
--- server.rb
+++ server.rb
@@ -7,5 +7,17 @@ set :environment, :production
 
 post '/diff' do
   content_type "text/html"
-  diff(params["html1"], params["html2"])
+
+  html1 = params["html1"] || ""
+  html2 = params["html2"] || ""
+
+  if html1.instance_of?(Hash)
+    html1 = html1[:tempfile].read
+  end
+
+  if html2.instance_of?(Hash)
+    html2 = html2[:tempfile].read
+  end
+
+  diff html1, html2
 end
